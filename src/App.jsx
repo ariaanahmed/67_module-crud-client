b44e767078f7128b3ecc991d4e86d93b6d5412cb
ariaan ahmed <you@example.com>
2023-05-07 16:01:52 +0600

    class3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
 const App = () => {
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+
+    console.log(user)
+
+    fetch('http://localhost:4000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+
+    }).then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
+  // 09:18
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h2>Simgple CRUD</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" /> <br />
+        <input type="email" name="email" placeholder="Your email" /> <br />
+        <input type="submit" value="Add user" />
+      </form>
+    </>
   );
 };
 
